7aec10675ef1ab2f4e5acce7a9e386a5e6442dd0
Garrett Jackson <garrettjackson43@gmail.com>
2022-11-14 10:24:26 -0500

    updates to squl

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,22 @@
 INSERT INTO Department (id, department_name,)
-VALUES ("spinach", "produce"),
-       ("peanut butter", "staples"),
-       ("peas-canned", "canned goods"),
-       ("ice cream", "frozen"),
-       ("potato chips", "snacks");
+VALUES (1, "produce"),
+       (2, "bakery"),
+  
+      
 
            
-INSERT INTO roles (id, title, salary, department_id)
-VALUES ("spinach", "produce"),
-       ("peanut butter", "staples"),
-       ("peas-canned", "canned goods"),
-       ("ice cream", "frozen"),
-       ("potato chips", "snacks");
+INSERT INTO roles (id, title, salary, )
+VALUES (1, "Manager" , "44k"),
+       (2, "assistant Manager" , "35k"),
+       (3, "Employee" , "20k"),
+ 
        
 
 
-INSERT INTO employees (first_name, last_name,role_id, manager_id)
-VALUES ("spinach", "produce"),
-       ("peanut butter", "staples"),
-       ("peas-canned", "canned goods"),
-       ("ice cream", "frozen"),
-       ("potato chips", "snacks");
+INSERT INTO employees (first_name, last_name,role_id,department_id)
+VALUES ("Sam", "Smith" , 1, 2 ),
+       ("Bruce", "Kent" , 2,2),
+       ("Clark", "Wayne" , 3,2),
+       ("Wally", "Jones" , 1,1),
+       ("John", "West" , 1,3);
        
